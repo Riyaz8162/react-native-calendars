f6ae643ffabda58da166241e3af19066a3573ec4
ASPIRESYS\mohamed.riyas <mohamed.aspire@gemseducation.com>
2022-05-10 09:09:13 +0530

    timeline 0 append

--- src/timeline/TimelineHours.tsx
+++ src/timeline/TimelineHours.tsx
@@ -37,15 +37,15 @@ const TimelineHours = (props: TimelineHoursProps) => {
       if (i === start) {
         timeText = '';
       } else if (i < 12) {
-        timeText = !format24h ? `${i} AM` : `${i}:00`;
+        timeText = !format24h ? `${i<=9?'0'+i:i}:00 AM` : `${i<=9?'0'+i:i}:00`;
       } else if (i === 12) {
-        timeText = !format24h ? `${i} PM` : `${i}:00`;
+        timeText = !format24h ? `${i}:00 PM` : `${i<21?'0'+i:i}:00`;
       } else if (i === 24) {
-        timeText = !format24h ? '12 AM' : '23:59';
+        timeText = !format24h ? '12:00 AM' : '23:59';
       } else {
-        timeText = !format24h ? `${i - 12} PM` : `${i}:00`;
+        timeText = !format24h ? `${((i- 12)<=9) ?'0'+(i-12)+':00':(i-12)+':00'} PM` : `${i}:00`;
       }
-      return {timeText, time: i};
+      return {timeText, time:i};
     });
   }, [start, end, format24h]);
 
